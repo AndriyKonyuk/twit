f22620637e976c82fb3bf3e3c9d2a8356de36c1b
andrii <andriykonyuk@gmail.com>
2018-01-16 13:38:37 +0200

    Heroku deploy.

--- twit/settings.py
+++ twit/settings.py
@@ -130,7 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'twit/staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'twitter_monitoring/static'),
+)
 
 from celery.schedules import crontab
 
